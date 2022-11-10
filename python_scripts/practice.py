0e9e224906bf18e666c21cdddec56d80394b5be4
Simone G <simonegordon12@icloud.com>
2022-11-10 19:49:47 +0000

    minor adjustments

---
+++ python_scripts/practice.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+DD = [1, 2, 3]
+spheres = np.zeros((len(DD)))
+print(spheres)
--- python_scripts/star-find-formation-pos.py
+++ python_scripts/star-find-formation-pos.py
@@ -1,31 +1,27 @@
 import yt
 import ytree
-import numpy as np
-import matplotlib.pyplot as plt
 from yt.extensions.p2p import add_p2p_fields, add_p2p_particle_filters
+import os
 
 
 # Load last star formation snapshot and add pop3/metallicity fields+filters
-
-ds = yt.load("DD0122/DD0122")
+root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/BF-0.0078125"
+ds = yt.load(os.path.join(root_dir, "DD0129/DD0129"))
 add_p2p_fields(ds)
-add_p2p_particle_filters(ds)
-
+#add_p2p_particle_filters(ds)
 
 # Set units of parameters
-
 masses = ds.r['particle_mass'].to('Msun')
 positions = ds.r['particle_position'].to('unitary')
 
+# Load seed1 merger tree of dataset (up to DD0118 in gas run)
+a = ytree.load('/home/sgordon/disk14/pop3/gas+dm-L3/rockstar_halos/out_0.list')
+a1 = ytree.load('/home/sgordon/disk14/pop3/gas+dm-L3/tree_810/tree_810.h5')
+r_halo = a1[0]["virial_radius"].to('pc')
+r = ds.quan(r_halo.d, "pc") # virial radius
 
-# Load merger tree of dataset (up to DD0118 in gas run)
-a = ytree.load('../gas+dm-L3/rockstar_halos/out_0.list')
 
-
-## Make Sphere of Most Massive Halo ##
-
-# Load my_tree
-a1 = ytree.load('../gas+dm-L3/tree_810/tree_810.h5')
+"""Make Sphere of Most Massive Halo"""
 
 # Find position and virial radius of the most massive halo
 pos_halo = a1[0]["position"].to('unitary')
@@ -34,19 +30,17 @@ p = ds.arr(pos_halo.d, "unitary") # position
 r_halo = a1[0]["virial_radius"].to('pc')
 r = ds.quan(r_halo.d, "pc") # virial radius
 
-
 # Make sphere containing the most massive halo centred on most dense point, c, with radius = virial radius
-
 v, c = ds.find_max('density') # c is position, v is max density
 sp = ds.sphere(c, r)
 
-
 # Print out Pop III star mass and position and time of formation
+star_mass = sp['SmartStar', 'particle_mass']
+star_pos = sp['SmartStar', 'particle_position']
+creation_time = sp['SmartStar', 'creation_time']
 
-star_mass = sp['pop3', 'particle_mass']
-star_pos = sp['pop3', 'particle_position']
-creation_time = sp['pop3', 'creation_time']
-
-print(star_mass)
-print(star_pos)
-print(creation_time)
+print("==========================================")
+print("BH Mass: ", star_mass)
+print("position: ", star_pos)
+print("creation time: ", creation_time)
+print("==========================================")
