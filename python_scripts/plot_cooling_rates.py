a1cd777fdb0e4cbee6d50c8675a9cf647d1ff147
Simone G <simonegordon12@icloud.com>
2023-03-09 16:01:04 +0000

    finalised cooling rates ratio plotting script - need to add Toomre Q parameter

--- python_scripts/plot_cooling_rates.py
+++ python_scripts/plot_cooling_rates.py
@@ -10,42 +10,79 @@ import numpy as np
 from smartstar_find import ss_properties
 import matplotlib.pyplot as plt
 from derived_fields import add_fields_ds
+from find_disc_attributes import make_disk
 
 # set by user
-root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSb01-2"
+root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSm16-2"
 input = sys.argv[1]
 ds = yt.load(os.path.join(root_dir, sys.argv[1]))
+
+# add all cooling rate derived fields to ds
 add_fields_ds(ds)
 ad = ds.all_data()
 
-q_time = ad['enzo', 'Cooling_Time'] # [s]
-print("q_time: ", q_time)
-q_energy = ad['enzo', 'GasEnergy'].to('ergs/g') # g cm^2 s^-2 cm^-3 = g cm^-1 s^-2 = ergs g^-1
-print("q_energy: ", q_energy)
+# define cooling rates
+specific_energy = ad['enzo', 'GasEnergy'].to('ergs/g') # g cm^2 s^-2 cm^-3 = g cm^-1 s^-2 = ergs g^-1
+print("q_energy: ", specific_energy)
 
+r = ad["index", "radius"].to('pc')
+q_sim = ad["enzo", "simulation_cooling_rate"] # [erg g^-1 s^-1] = (g cm^2 s^-2) g^-1 s^-1 = cm^2 s^-3
+q_rad = ad["enzo", "radiative_cooling_rate"]
+q_adv = ad["enzo", "advective_cooling_rate"]
 
-print("cooling rate: ", ad["enzo", "cooling_rate"])
-
-q_sim = ad["enzo", "cooling_rate"] # [erg g^-1 s^-1] = (g cm^2 s^-2) g^-1 s^-1 = cm^2 s^-3
-n = ad["gas", "H_nuclei_density"] # cm^-3
-rho = ad["gas", "density"] # g/cm^-3
-v_r = ad["gas", "radial_velocity"].to('cm/s')
-temp = ad["gas", "temperature"]
-r = ad["index", "radius"].to('cm')
-
-# constants
-kb = 1.3807e-16*((yt.units.cm**2 * yt.units.g)/(yt.units.s**2 * yt.units.K)) # cm2 g s-2 K-1
-m_p = 1.6726e-24*yt.units.g # g
-xi = -0.65 # dimensionless parameter following Chen et al. (1995)
-# fh2 = ? # molecular fraction - may not need this
-print("kb ", kb)
-
-# radiative cooling rate [erg s-1 cm-2]
-#q_rad = (q_sim * n * sigma * fh2**2) / m_p
-
-q_rad = q_sim * rho # [erg s^-1 cm^-3]
+# radiative cooling rate [erg s-1 cm-3]
 print("q_rad: ", q_rad)
 
 # advective cooling rate [erg s-1 cm-3]
-q_adv = ((rho * v_r * temp * kb * xi) / (r * m_p)).to('erg/((cm**3)*s)')
 print("q_adv: ", q_adv)
+
+# set up figure
+fig = plt.figure()
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.default"] = "regular"
+linewidth = 1
+plt.rcParams['lines.linewidth'] = linewidth
+
+fontsize = 10
+font = {'family': 'serif',
+        'weight': 'normal',
+        'size': fontsize,
+        }
+plt.xlabel(r"$Radius \, (pc)$", fontdict=font)
+plt.ylabel(r"$Q_{adv} / Q_{rad}$", fontdict=font)
+
+# make profile from disk object
+ss_pos, ss_mass, ss_age = ss_properties(ds)
+disk = make_disk(ss_pos, 10, 0.2)
+profile = yt.create_profile(
+    data_source=disk,
+    bin_fields=[("index", "radius")],
+    fields=[("enzo", "radiative_cooling_rate"), ("enzo", "advective_cooling_rate")],
+    n_bins=260,
+    units=dict(radius="pc"),
+    logs=dict(radius=True),
+    weight_field=("gas", "mass"),
+)
+
+# plot and save figure
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+plt.loglog(profile.x[profile.used], profile[("enzo", "radiative_cooling_rate")][profile.used] /
+           profile[("enzo", "advective_cooling_rate")][profile.used])
+plt.axhline(y=1, color='black', linestyle='dashed', lw=linewidth, alpha=1)
+
+# naming plot
+seed = int(root_dir[43:44])
+print(seed)
+if seed == 1:
+    index = 82
+elif seed == 2:
+    index = 84
+
+plot_name = 'radial-profile-plot-cooling-rate-' + str(root_dir[index:]) + '_' + str(input)[7:] + '.pdf'
+plt.savefig('plots/' + plot_name, dpi=100)
+print("created ", 'plots/' + plot_name)
