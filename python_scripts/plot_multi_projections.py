6a06a8878d148deb1c67d87cd6a5bf933ca0b8d7
Simone G <simonegordon12@icloud.com>
2023-04-05 09:29:35 +0100

    add simulation labels and cbar minorticks to multipanel projection plot

--- python_scripts/plot_multi_projections.py
+++ python_scripts/plot_multi_projections.py
@@ -1,33 +1,51 @@
-from smartstar_find import ss_properties
-import matplotlib.pyplot as plt
+########################################  MultiPanel Projections  ########################################
+#
+# to run: python plot_multipanel_projections.py [width index k]
+##########################################################################################################
+
+import os
+import sys
+import yt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from matplotlib import pyplot, colors
 from mpl_toolkits.axes_grid1 import AxesGrid
+from smartstar_find import ss_properties
+
+from matplotlib.ticker import FuncFormatter
 import yt.visualization.eps_writer as ytvis
 import matplotlib as mpl
-from matplotlib import pyplot, colors
-from matplotlib.ticker import FuncFormatter
-import numpy as np
-import os
 from matplotlib.colors import LogNorm
-import yt
+
+
+def tidy_data_labels(labels):
+    # for lists of labels
+    if len(labels) < 5:
+        data_labels = [i.replace("-2", "") for i in labels]
+        data_labels = [i.replace("RS", "") for i in data_labels]
+    # for single label
+    else:
+        data_labels = labels.replace("-2", "")
+        data_labels = data_labels.replace("RS", "")
+    return data_labels
 
 
 pyplot.rcParams['font.size'] = 12
-
-from grid_figure import GridFigure
-from yt.utilities.cosmology import Cosmology
-from yt.utilities.physical_constants import G
-from yt.visualization.color_maps import yt_colormaps
-
+pyplot.rcParams['font.weight'] = 'light'
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('font', **{'family': 'serif', 'serif': ['Times'], 'weight': 'light'})
+rc('text', usetex=True)
 
 # set by user
 root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/"
 sim = ["1B.RSm01", "1B.RSm04-2", "1B.RSm08-2"]
 dds = ["DD0148/DD0148", "DD0148/DD0148", "DD0238/DD0238"]
 
-# n_col = n_row = 3
-# fig, axs = plt.subplots(nrows=n_row, ncols=n_col, figsize=(9, 9))
+
 fig = plt.figure()
+
+fontproperies = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 12}
 
 grid = AxesGrid(
     fig,
@@ -41,7 +59,7 @@ grid = AxesGrid(
     cbar_location="right",
     cbar_mode="single",
     cbar_size="3%",
-    cbar_pad="1%",
+    cbar_pad="0%",
 )
 
 proj = []
@@ -49,19 +67,22 @@ labels = []
 DS = []
 for i, dd in enumerate(dds):
 
+    # data and data labels
     ds = yt.load(os.path.join(root_dir, sim[i], dd))
-    label = "s1_" + str(float(ds.current_time.to('Myr')))[:5] + "_Myr"
+    label = tidy_data_labels(sim[i])
     DS.append(ds)
     labels.append(label)
 
+    # grab bh properties and set projection dimensions
     field = "number_density"
     ss_pos, ss_mass, ss_age = ss_properties(ds)
     center = ss_pos
-    widths = [150, 40, 20] * ds.units.pccm
+    widths = [150, 40, 10] * ds.units.pccm
     r = 2000  # pc
     sp = ds.sphere(center, 2 * r)
     fontsize = 10
-    width = widths[0]
+    k = sys.argv[-1]
+    width = widths[k]
 
     # projection plot
     p = yt.ProjectionPlot(ds, "x", ("gas", field), width=width, center=center,
@@ -75,14 +96,33 @@ for i, dd in enumerate(dds):
     p.hide_colorbar()
 
     # annotate projection
-    if i == 0:
+    if (i == 0) and (k == 2):
         p.annotate_timestamp(corner='lower_right', redshift=True, draw_inset_box=True)
-    if i == 2:
+    if k == 0:
+        # transaxes = grid[i].axes.transAxes
+        # props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        # grid[i].axes.text(0.05, 0.95, label, transform=transaxes, fontsize=14,
+        #                   verticalalignment='top', bbox=props)
+        p.annotate_text(
+            [0.08, 0.08],
+            str(label),
+            coord_system="axis",
+            text_args={"color": "black"},
+            inset_box_args={
+                "boxstyle": "square,pad=0.3",
+                "facecolor": "white",
+                "linewidth": 3,
+                "edgecolor": "white",
+                "alpha": 0.5,
+            },
+        )
+    if i == 1:
         p.annotate_scale(corner='lower_right', coeff=1, unit='pccm')
 
     p.annotate_text((0.17, 0.90), "BH Mass: {:.2f} Msun".format(ss_mass.d), coord_system="axis",
                     text_args={"color": "white"})
-    p.annotate_marker(center, coord_system="data", color="white")  # mark ss position
+    if k == 2:
+        p.annotate_marker(center, coord_system="data", color="white")  # mark ss position
     # p.annotate_streamlines(("gas", "relative_velocity_x"), ("gas", "relative_velocity_y"))
     p.annotate_title("BH Age = {:.2f} kyrs".format(ss_age[0] / 1e3))
 
@@ -90,10 +130,9 @@ for i, dd in enumerate(dds):
     plot = p.plots[("gas", field)]
     plot.figure = fig
     plot.axes = grid[i].axes
-    #plot.cax = grid.cbar_axes[i]
     # if j in [1, 2]:
     #     grid[i * 3 + j].set_aspect(2)
-    if i == 0:
+    if k == 2:
         plot.cax = grid.cbar_axes[i]
 
     # redraw the plot
@@ -101,12 +140,17 @@ for i, dd in enumerate(dds):
 
     # Modify the axes properties **after** p.render() so that they
     # are not overwritten.
-    if i == 0:
+    if k == 2:
         ticklabels = grid.cbar_axes[i].get_yticklabels()
         grid.cbar_axes[i].set_yticklabels(ticklabels, fontsize=12)
-        grid.cbar_axes[i].set_ylabel(r'$\rm Number \, Density \, \left(cm^{-3}\right)$', fontsize=12)
+        grid.cbar_axes[i].set_ylabel(r'Number Density \big($\rm cm^{-3}$\big)', fontsize=12)
         grid.cbar_axes[i].minorticks_on()
-
+        # make minorticks
+        a1 = np.logspace(1, 9, 9)
+        a2 = np.arange(1, 10, 1)
+        minorticks = np.outer(a1, a2).flatten()
+        grid.cbar_axes[i].set_yticks(minorticks, minor=True)
+        grid.cbar_axes[i].tick_params(labelsize=12)
     #plot.axes.xaxis.set_minor_locator(plt.LogLocator(base=10.0, subs=[2.0, 5.0, 8.0]))
 
-plt.savefig("multiplot_axesgrid.pdf")
+plt.savefig(f"multiplot_axesgrid_{widths.d[k]}pccm.pdf")
