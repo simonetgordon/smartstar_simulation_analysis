5de294867a02f2aa5035a435209ac96b4e623287
Simone G <simonegordon12@icloud.com>
2023-04-14 00:28:50 +0100

    final 4x6 plot_multipanel_time.py

--- python_scripts/plot_multipanel_time.py
+++ python_scripts/plot_multipanel_time.py
@@ -44,8 +44,8 @@ fontsize = 14 # for projection annotations
 fig = plt.figure()
 grid = AxesGrid(
     fig,
-    (0.01, 0.01, 0.55, 1.2),
-    nrows_ncols=(len(dds), len(sim)),
+    (0.01, 0.01, 0.75, 1.15),
+    nrows_ncols=(len(dds), len(sim)*2),
     axes_pad=0,
     label_mode="L",
     aspect=False,
@@ -65,119 +65,127 @@ max_n_index = int(np.log10(max_n))
 min_n_index = int(np.log10(min_n))
 
 # iterate over datadumps
-for i, dd in enumerate(dds):
+for s,_ in enumerate(sim):
+    for i, dd in enumerate(dds):
 
-    # make dataset and simulation label
-    ds = yt.load(os.path.join(root_dir, sim[0], dd))
-    label = tidy_data_labels(sim[0])
+        # make dataset and simulation label
+        ds = yt.load(os.path.join(root_dir, sim[s], dd))
+        label = tidy_data_labels(sim[s])
 
-    # grab bh properties and make sphere centered on BH
-    ss_pos, ss_mass, ss_age = ss_properties(ds)
-    center = ss_pos
-    r = 2000*yt.units.pc
-    sp = ds.sphere(center, 2 * r)
- 
-    # make disk data container and define angular momentum vector L
-    disc_r_pc = 2.1
-    disc_h_pc = 2.1
-    disk, L = _make_disk_L(ds, ss_pos, disc_r_pc, disc_h_pc)
+        # grab bh properties and make sphere centered on BH
+        ss_pos, ss_mass, ss_age = ss_properties(ds)
+        center = ss_pos
+        r = 2000*yt.units.pc
+        sp = ds.sphere(center, 2 * r)
+    
+        # make disk data container and define angular momentum vector L
+        disc_r_pc = 2.1
+        disc_h_pc = 2.1
+        disk, L = _make_disk_L(ds, ss_pos, disc_r_pc, disc_h_pc)
 
-    # Gives a 3d vector and it will return 3 orthogonal vectors, the first one being the original vector
-    # and the 2nd and 3rd being two vectors in the plane orthogonal to the first and orthogonal to each other.
-    # It's very useful for giving you a vector edge-on to the disk.
-    vecs = ortho_find(L)
-    v = 0
+        # Gives a 3d vector and it will return 3 orthogonal vectors, the first one being the original vector
+        # and the 2nd and 3rd being two vectors in the plane orthogonal to the first and orthogonal to each other.
+        # It's very useful for giving you a vector edge-on to the disk.
+        vecs = ortho_find(L)
+        v = 0
 
-    # set projection width in pc
-    width = 1.5 * ds.units.pc
+        # set projection width in pc
+        width = 1.5 * ds.units.pc
 
-    for j in np.arange(0,2):
-        # make projection plot
-        if j == 0:
-            north = vecs[1]
-            p = yt.ProjectionPlot(ds, vecs[v], ("gas", field), weight_field=("gas", "density"), north_vector=north, 
-                        center=disk.center, width=width, data_source=disk)
+        if s == 0:
+            js = [0, 1]
         else:
-            north = vecs[0]
-            p = yt.ProjectionPlot(ds, vecs[v+2], ("gas", field), weight_field=("gas", "density"), north_vector=north, 
-                                  center=disk.center, width=2 * disk.radius, data_source=disk)
-        p.set_axes_unit('pc')
-        p.set_font_size(fontsize)
+            js = [2, 3]
 
-        # Ensure the colorbar limits match for all plots
-        p.set_cmap(field, 'viridis')
-        p.set_zlim(("gas", field), min_n, max_n)
-        p.hide_colorbar()
+        for j in js:
+            # make projection plot
+            if (j == 0) or (j == 2):
+                north = vecs[1]
+                p = yt.ProjectionPlot(ds, vecs[v], ("gas", field), weight_field=("gas", "density"), north_vector=north, 
+                            center=disk.center, width=width, data_source=disk)
+            else:
+                north = vecs[0]
+                p = yt.ProjectionPlot(ds, vecs[v+2], ("gas", field), weight_field=("gas", "density"), north_vector=north, 
+                                    center=disk.center, width=2 * disk.radius, data_source=disk)
+            p.set_axes_unit('pc')
+            p.set_font_size(fontsize)
 
-        # Annotations
+            # Ensure the colorbar limits match for all plots
+            p.set_cmap(field, 'viridis')
+            p.set_zlim(("gas", field), min_n, max_n)
+            p.hide_colorbar()
 
-        # mark BH position
-        p.annotate_marker(center, coord_system="data", color="white")
+            # Annotations
 
-        # this forces the ProjectionPlot to redraw itself on the AxesGrid axes.
-        plot = p.plots[("gas", field)]
-        plot.figure = fig
-        k = 2*i + j # grid index
-        plot.axes = grid[k].axes
+            # mark BH position
+            p.annotate_marker(center, coord_system="data", color="white")
 
-        # first row and every second column only
-        if (i == 0) and (j in [0, 2]):
-            #p.annotate_title(str(label), font=16)
-            grid[k].set_title(str(label), fontsize=18)
+            # this forces the ProjectionPlot to redraw itself on the AxesGrid axes.
+            plot = p.plots[("gas", field)]
+            plot.figure = fig
+            k = 4*i + j # grid index
+            plot.axes = grid[k].axes
 
-        # first column only
-        if j == 0:
-            # age, mass and simulation label in first
-            p.annotate_text((0.40, 0.88), r"BH Mass: {} $\rm M_\odot$".format(int(ss_mass.d)), coord_system="axis", text_args={"color": "white", "fontsize": 10})
-            
-            # make colorbar
-            plot.cax = grid.cbar_axes[k]
-            #grid.cbar_axes[k].set_ymargin(-0.1)
-            #grid.cbar_axes[k].set_anchor((0.6, 0.2))
+            # first column only
+            if (j == 0) or (j == 2):
+                # age, mass and simulation label in first
+                #p.set_font_size(10)
+                p.annotate_text((0.07, 0.86), r"BH Mass: {} $\rm M_\odot$".format(int(ss_mass.d)), 
+                                coord_system="axis", text_args={"color": "white", "fontsize": 8})
+                #p.set_font_size(fontsize)
+                
+                # make colorbar
+                plot.cax = grid.cbar_axes[k]
+                #grid.cbar_axes[k].set_ymargin(-0.1)
+                #grid.cbar_axes[k].set_anchor((0.6, 0.2))
 
-        p.render()
+            p.render()
 
+            # Modify the scalebar, colorbar and axes properties **after** p.render() so that they are not overwritten.
 
-        # Modify the scalebar, colorbar and axes properties **after** p.render() so that they are not overwritten.
+            # first row and every second column only
+            if (i == 0) and ((j == 0) or (j == 2)):
+                #p.annotate_title(str(label), font=16)
+                grid[k].axes.set_title(str(label), fontsize=18)
 
-        # ticks + ticklabels
-        grid[k].axes.set_xticks([])
-        grid[k].axes.set_yticks([])
-        grid[k].axes.minorticks_on()
-        xticks = [-0.6, -0.4, -0.2, 0.0, 0.2, 0.4, 0.6]
-        grid[k].axes.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-        grid[k].axes.yaxis.set_minor_locator(ticker.AutoMinorLocator())
-        grid[k].axes.set_xticks(xticks, major=True, crs=plot)
-        grid[k].axes.set_yticks(xticks, major=True, crs=plot)
-        grid[k].axes.set_xticklabels([str(x) for x in xticks])
-        grid[k].axes.set_yticklabels([str(x) for x in xticks])
+            # ticks + ticklabels
+            grid[k].axes.set_xticks([])
+            grid[k].axes.set_yticks([])
+            grid[k].axes.minorticks_on()
+            xticks = [-0.6, -0.4, -0.2, 0.0, 0.2, 0.4, 0.6]
+            grid[k].axes.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+            grid[k].axes.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+            grid[k].axes.set_xticks(xticks, major=True, crs=plot)
+            grid[k].axes.set_yticks(xticks, major=True, crs=plot)
+            grid[k].axes.set_xticklabels([str(x) for x in xticks], rotation=90)
+            grid[k].axes.set_yticklabels([str(x) for x in xticks])
 
-        if j == 0:
-            grid[k].axes.set_ylabel("{:.2f}".format(ds.current_time.to('Myr')))
+            if j == 0:
+                grid[k].axes.set_ylabel("{:.2f}".format(ds.current_time.to('Myr')))
 
-        if i == (len(dds)-1): #temp 2 -> 0
+            if i == (len(dds)-1): #temp 2 -> 0
 
-            # ylabel
-            grid[k].axes.set_xlabel("(pc)")
+                # ylabel
+                grid[k].axes.set_xlabel("(pc)")
 
-            # colorbar
-            grid.cbar_axes[k].set_ylabel(r'Number Density \big($\rm \frac{1}{cm^{3}}$\big)', fontsize=12)
-            grid.cbar_axes[k].minorticks_on()
+                # colorbar
+                grid.cbar_axes[k].set_ylabel(r'Number Density \big($\rm \frac{1}{cm^{3}}$\big)')
+                grid.cbar_axes[k].minorticks_on()
 
-            # make minorticks
-            a1 = np.logspace(min_n_index, max_n_index, np.abs(min_n_index - max_n_index)+1)
-            a2 = np.arange(1, 10, 1)
-            minorticks = np.outer(a1, a2).flatten()
-            atol = 0.9*10**(max_n_index)
-            end_i = first_index(minorticks, float(max_n.d), rtol=0.1, atol=atol)
-            minorticks = minorticks[:end_i]
-            grid.cbar_axes[k].xaxis.set_minor_locator(ticker.AutoMinorLocator())
-            grid.cbar_axes[k].set_yticks(minorticks, minor=True)
-            grid.cbar_axes[k].tick_params(labelsize=12)
+                # make minorticks
+                a1 = np.logspace(min_n_index, max_n_index, np.abs(min_n_index - max_n_index)+1)
+                a2 = np.arange(1, 10, 1)
+                minorticks = np.outer(a1, a2).flatten()
+                atol = 0.9*10**(max_n_index)
+                end_i = first_index(minorticks, float(max_n.d), rtol=0.1, atol=atol)
+                minorticks = minorticks[:end_i]
+                grid.cbar_axes[k].xaxis.set_minor_locator(ticker.AutoMinorLocator())
+                grid.cbar_axes[k].set_yticks(minorticks, minor=True)
+                grid.cbar_axes[k].tick_params(labelsize=fontsize)
 
-            ax_pos = grid[k].axes.get_position()
-            cb_pos = grid.cbar_axes[k].get_position()
-            #cb_pos = grid.cbar_axes[k].set_position([cb_pos.x0 - 0.1, ax_pos.y0, cb_pos.width, cb_pos.height])
+                ax_pos = grid[k].axes.get_position()
+                cb_pos = grid.cbar_axes[k].get_position()
+                #cb_pos = grid.cbar_axes[k].set_position([cb_pos.x0 - 0.1, ax_pos.y0, cb_pos.width, cb_pos.height])
 
 plot_name = f"multiplot_axesgrid_time_{width.d}pc.pdf"    
 plt.savefig('plots/'+ plot_name, bbox_inches='tight')
