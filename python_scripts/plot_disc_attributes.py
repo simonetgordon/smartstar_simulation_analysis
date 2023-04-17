b18f73d887322f787cdf9e825a60b0934a972803
Simone G <simonegordon12@icloud.com>
2023-04-17 22:12:44 +0100

    adding orbital velocity to disc attributes plot

--- python_scripts/plot_disc_attributes.py
+++ python_scripts/plot_disc_attributes.py
@@ -16,6 +16,22 @@ from matplotlib import rc
 from find_disc_attributes import _make_disk_L
 from plot_multi_projections import tidy_data_labels
 
+def orbital_velocity(ds, disk):
+    #G = ds.parameters['GravitationalConstant']
+    G = 6.67e-8 * (yt.units.cm ** 3)/(yt.units.g*yt.units.s**2) # cgs
+    return np.sqrt(G * ds.r['SmartStar', 'particle_mass'].to('g') / disk['index', 'radius'])
+
+def radial_profile(field, disk, n_bins, cell_width_pc):
+    bins = np.logspace(np.log10(cell_width_pc), np.log10(disk.radius.to('pc')), n_bins+1)
+    counts_r, r_bin_edges = np.histogram(disk["index", "radius"].to('pc'), bins=bins)
+    y, radius = np.histogram(disk["index", "radius"].to('pc'), weights=field, bins=bins)
+    y = np.nan_to_num(y)
+    y = y/counts_r
+    y = np.nan_to_num(y)
+    y_no_zeros = y[y > 0]
+    r_no_zeros = radius[:-1][y > 0]
+    return r_no_zeros, y_no_zeros
+
 
 # datasets
 input = sys.argv[-1]
@@ -26,12 +42,12 @@ dds = ["DD0144/DD0144", "DD0154/DD0154"]
 labels = []
 DS = []
 for i, dd in enumerate(dds):
- ds = yt.load(os.path.join(root_dir[i], sim[i], dd))
- add_fields_ds(ds)
- j = []
- label = str(sim[i]) + "-" + str(float(ds.current_time.to('Myr')))[:5] + "Myr"
- DS.append(ds)
- labels.append(label)
+    ds = yt.load(os.path.join(root_dir[i], sim[i], dd))
+    add_fields_ds(ds)
+    j = []
+    label = str(sim[i]) + "-" + str(float(ds.current_time.to('Myr')))[:5] + "Myr"
+    DS.append(ds)
+    labels.append(label)
 
 labels = tidy_data_labels(labels)
 
@@ -44,6 +60,23 @@ elif seed == 2:
 
 
 if __name__ == "__main__":
+
+    # font settings
+    fontsize = 12 # for projection annotations
+    linewidth = 2
+    mpl.rcParams['pgf.texsystem'] = 'pdflatex'
+    plt.rcParams['font.size'] = fontsize
+    plt.rcParams['font.weight'] = 'light'
+    rc('font', **{'family': 'serif', 'serif': ['Times'], 'weight': 'light'})
+    #rc('text', usetex=True)
+    plt.rcParams["mathtext.default"] = "regular"
+    mpl.rcParams['text.usetex'] = True
+    plt.rcParams['lines.linewidth'] = linewidth
+
+    # create figure
+    n_subplots = 7
+    fig, axs = plt.subplots(n_subplots, 1, sharex=True)
+
     for k, ds in enumerate(DS):
         # grab bh particle properties
         ss_pos, ss_mass, ss_age = ss_properties(ds)
@@ -66,12 +99,12 @@ if __name__ == "__main__":
                     ('gas', 'angular_frequency'), ('gas', 'velocity_spherical_theta'),
                     ('index', 'radius'), ('gas', 'keplerian_frequency_BH'), ("gas", "tangential_velocity"), ("index", "height"),
                     ('gas', 'velocity_spherical_phi'),
-                    ("gas", "omega"), ("gas", "omega_k")],
+                    ("gas", "omega"), ("gas", "omega_k"), ('gas', 'orbital_velocity')],
             n_bins=n_bins,
             units=dict(
                     radius="pc", velocity_cylindrical_theta="km/s", sound_speed="km/s", velocity_spherical_theta="km/s",
-                    cylindrical_z="pc", radial_velocity="cm/s", keplerian_frequency_BH="1/s", tangential_velocity="km/s",
-                    height="pc"),
+                    cylindrical_z="pc", radial_velocity="km/s", keplerian_frequency_BH="1/s", tangential_velocity="km/s",
+                    height="pc", orbital_velocity="km/s"),
             logs=dict(cylindrical_radius=False),
             weight_field=("gas", "cell_mass"),
         )
@@ -90,42 +123,33 @@ if __name__ == "__main__":
         #                                           Plot All Disc Attributes
         ##########################################################################################################
 
-        # font settings
-        fontsize = 12 # for projection annotations
-        linewidth = 2
-        mpl.rcParams['pgf.texsystem'] = 'pdflatex'
-        plt.rcParams['font.size'] = fontsize
-        plt.rcParams['font.weight'] = 'light'
-        rc('font', **{'family': 'serif', 'serif': ['Times'], 'weight': 'light'})
-        #rc('text', usetex=True)
-        plt.rcParams["mathtext.default"] = "regular"
-        mpl.rcParams['text.usetex'] = True
-        plt.rcParams['lines.linewidth'] = linewidth
-
-        # create figure
-        n_subplots = 6
-        fig, axs = plt.subplots(n_subplots, 1, sharex=True)
 
         # ignore invalid value error in plot_theta divide
         np.seterr(invalid='ignore')
 
         # define plots
         l = ["1Bb01", "1Sb01"]
+        c = ["blueviolet", "orangered"]
+        cell_width_pc = [0.003, 0.012]
         plot_omega = axs[0].loglog(profile.x[profile.used], profile[("gas", "omega")][profile.used] /
-                    profile[("gas", "omega_k")][profile.used])
-        plot_density = axs[1].plot(profile.x[profile.used], profile[("gas", "H_nuclei_density")][profile.used])
-        plot_temp = axs[2].loglog(profile.x[profile.used], profile[("gas", "temperature")][profile.used])
-        plot_h = axs[3].loglog(profile.x[profile.used], profile[("index", "height")][profile.used])
+                    profile[("gas", "omega_k")][profile.used], color=c[k], label=labels[k])
+        plot_density = axs[1].plot(profile.x[profile.used], profile[("gas", "H_nuclei_density")][profile.used], color=c[k])
+        plot_temp = axs[2].loglog(profile.x[profile.used], profile[("gas", "temperature")][profile.used], color=c[k])
+        plot_h = axs[3].loglog(profile.x[profile.used], profile[("index", "height")][profile.used], color=c[k])
         plot_theta = axs[4].plot(profile.x.value, np.abs(profile[("gas", "tangential_velocity")].value /
-                                                        profile[("gas", "sound_speed")].value))
-        plot_vr = axs[5].loglog(profile.x[profile.used], np.abs(profile[("gas", "radial_velocity")][profile.used]))
+                                                        profile[("gas", "sound_speed")].value), color=c[k])
+        plot_vr = axs[5].plot(profile.x[profile.used], profile[("gas", "radial_velocity")][profile.used], color=c[k])
+        r, vorb = radial_profile(orbital_velocity(ds, disk), disk, n_bins, cell_width_pc[k])
+        plot_vorb = axs[6].plot(r, vorb, color=c[k])
 
 
         # format plots
         axs[-1].set_xlabel("Radius (pc)", fontdict=None)
+        axs[6].set_ylabel(r"$\rm \nu_{orbit} \, (km/s)$", fontdict=None)
         axs[5].set_ylabel(r"$\rm \nu_r \, (km/s)$", fontdict=None)
         axs[5].set_yscale('linear')
         axs[4].set_ylabel(r"$\rm \nu_{\theta} \,/ c_s$", fontdict=None)
+        axs[4].set_ylim([0.1,8])
         axs[3].set_ylabel(r"$\rm H \, (pc)$", fontdict=None)
         axs[2].set_ylabel(r"$\rm T \, (K)$", fontdict=None)
         axs[1].set_ylabel(r"$\rm n \, (cm^{-3})$", fontdict=None)
@@ -137,16 +161,16 @@ if __name__ == "__main__":
         #axs[0].set_title("BH Age = " + "{:.2f}".format(ss_age[0]/1e6) + " Myr" + ", " + str(root_dir[index:]))
 
         for i in range(n_subplots):
-            axs[i].set_xlim([7e-3, 1e1])
+            axs[i].set_xlim([1.5e-3, 1e1])
             axs[i].tick_params(axis="x", which='minor', length=2, direction="in")
             axs[i].tick_params(axis="x", which='major', labelsize=fontsize, width=1, length=3, direction="in")
             axs[i].tick_params(axis="y", which='major', labelsize=fontsize)
             axs[i].tick_params(axis="y", which='minor', labelsize=fontsize-2)
 
 
-        # save plot as pdf
-        fig.subplots_adjust(wspace=0, hspace=0)
-        fig.set_size_inches(6, 10)
-        plot_name = 'disc_rp_' + str(input) + '.pdf'
-        plt.savefig('plots/' + plot_name, bbox_inches='tight')
-        print("created plots/" + str(plot_name))
+    # save plot as pdf
+    fig.subplots_adjust(wspace=0, hspace=0)
+    fig.set_size_inches(5.8, 8)
+    plot_name = 'disc_rp_' + str(input) + '.pdf'
+    plt.savefig('plots/' + plot_name, bbox_inches='tight')
+    print("created plots/" + str(plot_name))
