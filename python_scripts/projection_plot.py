5f80633b689995a5fa2b5479805fc48b9a6d7ede
Simone G <simonegordon12@icloud.com>
2023-04-21 09:34:12 +0100

    trying out adding streamlines to projection plot

--- python_scripts/projection_plot.py
+++ python_scripts/projection_plot.py
@@ -4,19 +4,58 @@ python -i projection_plot.py DD0133/DD0133
 """
 
 import yt
+import shutil
 import sys
 import os
 from smartstar_find import ss_properties
 from find_disc_attributes import _make_disk_L
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from mpl_toolkits.axes_grid1 import AxesGrid
+import time
 
 # set by user
-w_pccm = 7
+w_pccm = 1.5
 field = "density"
 
 # set by user
-root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSm16-2"
+root_dir = "/home/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/40msun/replicating-beckmann/1S.RSb01"
+
+# ## First check there is a local /data area                                                                                                                                                                 
+# if not os.path.isdir("/data"):
+#     sys.exit("Error: no /data")
+
+# ## Second, check we have a directory. If not, then create one.                                                                                                                                             
+# UserName=os.getlogin()
+# LocalDir=os.path.join("/data",UserName)
+# if not os.path.isdir(LocalDir):
+#     print("Creating Directory "+LocalDir)
+#     os.mkdir(LocalDir)
+
+# ## Third, check if the data is already there, and if not, copy it over.                                                                                                                                    
+# DataDumpFull = sys.argv[1]
+# DataDump = DataDumpFull.split('/')
+# LocalData = os.path.join(LocalDir,DataDump[0])
+# if not os.path.isdir(LocalData):
+#     print("Copying data to "+LocalData)
+#     shutil.copytree(os.path.join(root_dir,DataDump[0]),LocalData)
+#     print("Done copying data")
+# else:
+#     print("Found a local copy in "+LocalData)
+
 input = sys.argv[1]
+
+# Load the data from the local directory                                                                                                                                                                   
 ds = yt.load(os.path.join(root_dir, sys.argv[1]))
+
+# naming plot
+seed = int(root_dir[43:44])
+print(seed)
+if seed == 1:
+    index = 82
+elif seed == 2:
+    index = 84
+
 
 # naming sphere container directory
 seed = int(root_dir[43:44])
@@ -27,19 +66,32 @@ elif seed == 2:
 sp_container_dir = root_dir[index:]
 
 # make sphere centred on current ss position
-width = (w_pccm, 'pccm')
+width = (w_pccm, 'pc')
 r = 2000 # pc
 ss_pos, ss_mass, ss_age = ss_properties(ds)
 center = ss_pos
+tic = time.perf_counter()
 sp = ds.sphere(center, 2*r)
 disk, L = _make_disk_L(ds, center, 0.5*yt.units.pc, 0.2*yt.units.pc)
+toc = time.perf_counter()
+print(f"Made the disc in {toc - tic:0.4f} seconds")
 
+# font settings
 fontsize = 26
+linewidth = 2
+plt.rcParams['font.size'] = fontsize
+plt.rcParams['font.weight'] = 'light'
+rc('font', **{'family': 'serif', 'serif': ['Times'], 'weight': 'light'})
+rc('text', usetex=True)
+
+plt.rcParams["mathtext.default"] = "regular"
+plt.rcParams['lines.linewidth'] = linewidth
 
 # Gas density
 if field == "density":
+    tic = time.perf_counter()
     field = "number_density"
-    p = yt.ProjectionPlot(ds, L, ("gas", field), width=width, center=center, data_source=sp,
+    p = yt.ProjectionPlot(ds, 'x', ("gas", field), width=width, center=center, data_source=sp,
                           weight_field='density')
     p.set_cmap(field, 'viridis')
     p.set_font_size(fontsize)
@@ -48,6 +100,9 @@ if field == "density":
 
     # annotate
     #p.annotate_scale(corner='lower_right')
+    p.annotate_streamlines(("gas", "velocity_y"), ("gas", "velocity_z"), density = 0.7, linewidth=0.6, color='white')
+                           #field_color=("gas", "velocity_x")
+                           # )
     p.annotate_timestamp(corner='lower_right', redshift=True, draw_inset_box=False)
     p.annotate_text((0.55, 0.94), r"BH Mass: {:.2f} $\rm M_\odot$".format(ss_mass.d), coord_system="axis",
                     text_args={"color": "white"})
@@ -59,7 +114,7 @@ if field == "density":
     #p.annotate_title("BH Age = {:.2f} kyrs".format(ss_age[0]/1e3))
 
     # save
-    plot_name = 'density-' + str(root_dir[82:]) + '-' + str(input)[10:] + '-' + str(w_pccm) + 'pccm.png'
+    plot_name = 'density-' + str(root_dir[81:]) + '-' + str(input)[10:] + '-' + str(w_pccm) + 'pccm.png'
     p.save('density_plots/' + plot_name)
     print("created density_plots/" + str(plot_name))
 
