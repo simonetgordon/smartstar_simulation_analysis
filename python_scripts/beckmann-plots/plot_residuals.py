3d37e6d96bdfc0367fbf848bcf4e87a425d9d97b
Simone G <simonegordon12@icloud.com>
2023-05-08 09:27:24 +0100

    updates to plotting residuals - change to fractional difference

--- python_scripts/beckmann-plots/plot_residuals.py
+++ python_scripts/beckmann-plots/plot_residuals.py
@@ -1,6 +1,6 @@
 ########################################       Plot Residuals     ########################################
 #
-# to run: python plot_residuals.py [csv1 - baseline] [csv2] [csv3] [output_plotname]
+# to run: python plot_residuals.py [csv1 - baseline] [csv2] [csv3] .. [output_plot_name]
 ##########################################################################################################
 import yt
 import sys
@@ -25,7 +25,7 @@ def interpolate_data(arr, N=1000):
     return interp_arr
 
 
-def first_index(a, val, rtol=0.001, atol=10):
+def first_index(a, val, rtol=0.00001, atol=10): # rtol=0.001, atol=10)
     return next(i for i, _ in enumerate(a) if np.isclose(_, val, rtol, atol))
 
 
@@ -61,7 +61,7 @@ if __name__ == "__main__":
 
     # initialise figure
     baseline_sim = sys.argv[1][16:24]
-    plot_name = "residuals-" + acc_scheme + "-plot-baseline-" + str(baseline_sim) + ".pdf"
+    plot_name = "residuals-" + acc_scheme + "-plot-baseline-" + str(baseline_sim) + "-2.pdf"
     n_subplots = 4
     fig = plt.figure()
     fig, axs = plt.subplots(n_subplots, 1, sharex=True)
@@ -70,10 +70,10 @@ if __name__ == "__main__":
     c = ['dodgerblue', 'limegreen', 'crimson', 'salmon', 'lightgreen', 'khaki', 'plum', 'seagreen', 'steelblue', 'salmon']
 
     # define baseline age and accrate lines + number of data points
-    i_start = 100 # starting index
+    i_start = 1 # starting index
     baseline_age_raw = bhl_object_list[0].ages[i_start:]
     n_data_max = baseline_age_raw.shape[0]
-    N = int(n_data_max/100)
+    N = int(n_data_max/10)
     baseline_age = interpolate_data(bhl_object_list[0].ages[i_start:], N=N)
     baseline_accrate = interpolate_data(bhl_object_list[0].accrates[i_start:], N=N)
     baseline_mass = interpolate_data(bhl_object_list[0].mass[i_start:], N=N)
@@ -89,9 +89,9 @@ if __name__ == "__main__":
         len(BHL.ages) == len(BHL.accrates)
 
         # find index of age that matches end age of baseline
-        i_age = first_index(BHL.ages[i_start:], baseline_age[-1], atol=10)
+        i_age = first_index(BHL.ages[i_start:], baseline_age[-1], atol=50)
 
-        print("new final age accuracy: ", 1-np.abs(BHL.ages[i] - baseline_age[-1])/baseline_age[-1])
+        print("new final age accuracy %: ", (1-(np.abs(BHL.ages[i] - baseline_age[-1])/baseline_age[-1]))*100)
 
         # truncate the array at this point
         trunc_age = BHL.ages[i_start:i_age]
@@ -110,6 +110,7 @@ if __name__ == "__main__":
         interp_accrate = interpolate_data(move_avg_accrate, N=N)
         interp_accrate_raw = interpolate_data(trunc_accrate, N=n_data_max)
         interp_mass = interpolate_data(trunc_mass, N=N)
+        interp_mass_raw = interpolate_data(trunc_mass, N=n_data_max)
 
         print("----------------------------------------")
 
@@ -121,8 +122,8 @@ if __name__ == "__main__":
         axs[0].plot(interp_age_raw / 1e6, interp_accrate_raw, color=c[i], label=data_labels[i + 1], alpha=0.2)
         axs[0].plot(move_avg_age / 1e6, move_avg_accrate, color=c[i], label=data_labels[i + 1])
         axs[1].plot(interp_age/1e6, residual, color=c[i], label=data_labels[i+1])
-        axs[2].plot(interp_age/1e6, residual_normed, color=c[i], label=data_labels[i+1])
-        axs[3].plot(interp_age/1e6, interp_mass, color=c[i], label=data_labels[i+1])
+        axs[2].plot(interp_age/1e6, np.abs(residual_normed), color=c[i], label=data_labels[i+1]) # take abs for logscale
+        axs[3].plot(interp_age_raw/1e6, interp_mass_raw, color=c[i], label=data_labels[i+1])
 
     # plot baselines in grey
     axs[0].plot(baseline_age / 1e6, baseline_accrate, color='grey', label=data_labels[0])
@@ -135,20 +136,21 @@ if __name__ == "__main__":
     axs[0].set_yscale('log')
     axs[1].set_ylabel(r"$\rm \Delta \dot{M} \, (M_{\odot}/yr)$")
     #axs[0].set_xlabel(r"Time Since Formation (yr)", fontdict=font)
-    axs[3].legend()
+    axs[1].legend(loc="upper left")
     axs[2].set_ylabel(r"$\rm \Delta \dot{M} \, / \, \dot{M}_{m16}$")
+    axs[2].set_yscale('log')
     axs[3].set_ylabel(r"$\rm M \, (M_{\odot})$", fontdict=None)
     axs[3].set_yscale('log')
     axs[3].set_xlabel("BH Age (Myr)")
     for i in range(n_subplots):
-        axs[i].tick_params(axis="x", which='minor', length=4, direction="in")
-        axs[i].tick_params(axis="x", which='major', labelsize=fontsize, width=2, length=4, direction="in")
-        axs[i].tick_params(axis="y", which='major', labelsize=fontsize)
+        axs[i].set_xticks(np.arange(0, np.round(baseline_age[-1] / 1e6, 2), 0.1))
+        axs[i].minorticks_on()
+        axs[i].tick_params(axis="x", which='minor', length=2, direction="in")
+        axs[i].tick_params(axis="x", which='major', labelsize=fontsize, width=1.5, length=3, direction="in")
+        axs[i].tick_params(axis="y", which='major', labelsize=fontsize-1)
         axs[i].tick_params(axis="y", which='minor', labelsize=fontsize-2)
-        #axs[i].set_yscale('log')
-        #axs[i].set_xlim([0, xlim]) # for truncated view
 
     fig.subplots_adjust(wspace=0, hspace=0)
-    fig.set_size_inches(8, 8)
-    fig.savefig('plots/' + plot_name, dpi=100)
+    fig.set_size_inches(7, 7)
+    fig.savefig('plots/' + plot_name, bbox_inches='tight')
     print("created ", plot_name)
