738013deb5fb97308ebd2b7567f7a09584419327
Simone G <simonegordon12@icloud.com>
2023-05-08 09:14:51 +0100

    resolving conflict before stash apply

--- python_scripts/projection_movie.py
+++ python_scripts/projection_movie.py
@@ -8,20 +8,26 @@ import yt
 import os
 yt.enable_parallelism()
 from smartstar_find import ss_properties
+from plot_disc_projections import _make_disk_L
+from yt.utilities.math_utils import ortho_find
+from plot_multi_projections import tidy_data_labels
+#from yt.extensions.p2p import add_p2p_fields, add_p2p_particle_filters
 
 map = "density"
 # set by user
-root_dir = "~/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSm16"
+root_dir = "/disk14/sgordon/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSb01-2"
 enzo_file = "smartstar-production-runs.enzo"
 sim = os.path.join(root_dir, enzo_file)
 
 # naming sphere container directory
-seed = int(root_dir[43:44])
+seed = int(root_dir[38])
 if seed == 1:
     index = 82
 elif seed == 2:
     index = 84
 sp_container_dir = root_dir[index:]
+
+sim_str = tidy_data_labels(str(root_dir[76:]))
 
 if __name__ == "__main__":
     es = yt.load_simulation(sim, "Enzo", find_outputs=True)
@@ -38,8 +44,41 @@ if __name__ == "__main__":
         my_width = (right_edge - left_edge).max()
         center0 = 0.5 * (left_edge + right_edge)
 
+    ## find north vector a (near-end) ds
+    ds_final = yt.load(os.path.join(root_dir, "DD0300/DD0300"))
+    ss_pos, ss_mass, ss_age = ss_properties(ds_final)
+    center = ss_pos
+    r = 2000*yt.units.pc
+    sp = ds_final.sphere(center, 2 * r)
+
+    # make disk data container and define angular momentum vector L
+    disc_r_pc = 2.1
+    disc_h_pc = 2.1
+    disk, L = _make_disk_L(ds_final, ss_pos, disc_r_pc, disc_h_pc)
+    
+
+    # Gives a 3d vector and it will return 3 orthogonal vectors, the first one being the original vector
+    # and the 2nd and 3rd being two vectors in the plane orthogonal to the first and orthogonal to each other.
+    # It's very useful for giving you a vector edge-on to the disk.
+    vecs = ortho_find(L)
+    v = 0
+
+    # set image orientation + north vector
+    orient = "face-on"
+
+    if orient == "face-on":
+        orient_str = "face_on_"
+        dir = vecs[0]
+        north = vecs[1]
+    else:
+        orient_str = "edge_on_"
+        dir = vecs[1]
+        north = vecs[0]
     
     for ds in es.piter():
+
+        # add pop3 metallicity fields
+        #add_p2p_fields(ds)                                                                                                                                                                                
 
         ss_pos, ss_mass, ss_age = ss_properties(ds)
 
@@ -49,32 +88,46 @@ if __name__ == "__main__":
             width = (my_width, "unitary")
 
         center = ss_pos
-        w_pccm = 25
-        swap_axes = True
+        w_pccm = 30
+        w_pc = 50
+        #swap_axes = True
 
         if map == "density":
-            # Gas density
-            field = "H_nuclei_density"
-            p1 = yt.ProjectionPlot(ds, "z", ("gas", field), width=(w_pccm, 'pccm'), center=center, data_source=region,
-                                   weight_field=field)
+            # h number density
+            field = "number_density"
+            p1 = yt.ProjectionPlot(ds, dir, ("gas", field), width=(w_pc, 'pc'), north_vector=north, center=center, data_source=region,
+                                   weight_field=("gas", field))
             p1.set_cmap(field, 'viridis')
+            p1.set_zlim(("gas", field), 8e0, 9e7)
 
             # format
-            a = 0.70
+            a = 0.72
             b = 0.95
+            b2 = 0.03
             p1.set_axes_unit('pc')
-            if swap_axes:
-                p1.swap_axes()
-                a, b = b, a
-                p1.annotate_timestamp(x_pos=0.10, y_pos=0.87, redshift=True, draw_inset_box=True, coord_system="axis")
-            else:
-                p1.annotate_timestamp("lower_right", redshift=True, draw_inset_box=True, coord_system="axis")
-            p1.annotate_scale(corner='lower_left')
-            p1.annotate_marker(center, coord_system="data", color="black")  # mark ss position
-            p1.annotate_text((a, b), "Mass: {:.2f} Msun".format(ss_mass.d), coord_system="axis",
+
+            # annotations
+            # if swap_axes:
+            #     p1.swap_axes()
+            #     a, b = b, a
+            #     p1.annotate_timestamp(x_pos=0.10, y_pos=0.87, redshift=True, draw_inset_box=True, coord_system="axis")
+            # else:
+            #     p1.annotate_timestamp("lower_right", redshift=True, draw_inset_box=True, coord_system="axis")
+            #p1.annotate_scale(corner='lower_left')
+            p1.annotate_marker(center, coord_system="data", color="white")  # mark ss position
+            p1.annotate_text((a, b), r"BH Mass: {:.0f} $\rm M_\odot$".format(ss_mass.d), coord_system="axis",
                              text_args={"color": "white"})
-            p1.annotate_title("SS Age = {:.2f} kyrs, {} pccm across".format(ss_age[0] / 1e3, w_pccm))
-            dirname = "frames_edge_on_" + field + "_" + str(root_dir[75:]) + "/"
+            p1.annotate_text((a, b-0.05), "BH Age = {:.2f} Myr".format(ss_age[0] / 1e6), coord_system="axis",
+                             text_args={"color": "white"})
+            p1.annotate_text((0.86, b2), "z = {:.2f}".format(ds.current_redshift), coord_system="axis",
+                             text_args={"color": "white"})
+            #p1.annotate_title("{} pccm across".format(w_pccm))
+            p1.annotate_text([0.05, 0.05], sim_str, coord_system="axis", text_args={"color": "black"},
+                            inset_box_args={"boxstyle": "square,pad=0.3", "facecolor": "white", "linewidth": 3,
+                                            "edgecolor": "white", "alpha": 0.5},
+                            )
+
+            dirname = "frames_" + orient_str + field + "_" + sim_str + "_" + str(w_pc) + "pc/"
             p1.save(dirname)
         
         elif map == "temperature":
