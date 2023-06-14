863622e1be81f7a79d07401d8eed6844064da9ca
Simone G <simonegordon12@icloud.com>
2023-06-14 09:22:38 +0100

    plotting mass versus time for each accretion scheme -> moving from vertical to horizontal and adding text to figure 'low res -> high res'

--- python_scripts/beckmann-plots/plot_accretion_rates.py
+++ python_scripts/beckmann-plots/plot_accretion_rates.py
@@ -9,7 +9,7 @@ from plot_variables import tidy_data_labels, first_index, movingaverage
 """
 Produce all accretion rates for 1S group
 python -i plot_accretion_rates.py [csv1] [csv2] [csv3] [output_plotname e.g mass-flux-x4]
-
+List MF first, then BHL
 BEWARE: added 1st row of data-1S.RSm04.csv to data-1S.RSmf8.csv and data-1S.RSmf4.csv. -> need to all start from 10.8 msun.
 """
 
@@ -29,7 +29,7 @@ c = ['blueviolet', 'turquoise', 'limegreen', 'darkgreen']
 # set up figure
 fig = plt.figure()
 num_subplots = 2
-fig, axs = plt.subplots(num_subplots, 1, sharex=True)
+fig, axs = plt.subplots(1, num_subplots, sharey=True)
 
 # misc parameters
 l = tidy_data_labels(bhl_object_labels)
@@ -68,15 +68,14 @@ for j in range(num_subplots):
     # set same ylim for both
     axs[j].set_ylim(9.5, 2000)
 
-    # set same y-label for both
-    axs[j].set_ylabel(r"BH Mass ($\rm M_\odot$)", fontdict=None)
-
     # x-axis ticks
     axs[j].set_xticks(np.arange(0, time_cutoff, 0.1))
     axs[j].axhline(20, linestyle='dashed', color='grey', alpha=0.5)
     axs[j].minorticks_on()
     axs[j].xaxis.set_minor_locator(plt.MultipleLocator(0.02))
     axs[j].tick_params(axis="x", which='minor', length=2, direction="in")
+    axs[1].tick_params(axis="y", which='minor', length=2, direction="in")
+    axs[1].tick_params(axis="y", which='major', width=1, length=4, direction="in")
     axs[j].tick_params(axis="x", which='major', labelsize=fontsize, width=1, length=4, direction="in")
     axs[j].set_yscale('log')
     axs[j].legend(loc='upper left')
@@ -84,16 +83,25 @@ for j in range(num_subplots):
 
 # format individual subplots
 axs[0].set_title("Mass-Flux Accretion", fontdict=None)
+axs[0].set_xlabel("BH Age (Myr)", fontdict=None)
 axs[1].set_title("BHL Accretion", fontdict=None)
 axs[1].set_xlabel("BH Age (Myr)", fontdict=None)
+axs[1].text(0.17, 270, 'High Resolution', fontsize=12, color=c[-1])
+axs[1].text(0.17, 1200, 'Low Resolution', fontsize=12, color=c[0])
+# Add an arrow
+arrow_props = dict(facecolor='red', arrowstyle='->')
+axs[1].annotate('', xy=(0.24, 380), xytext=(0.24, 1100),
+            arrowprops=arrow_props)
+# set  y-label for both
+axs[0].set_ylabel(r"BH Mass ($\rm M_\odot$)", fontdict=None)
 
 
 # save plot as pdf
 fig = plt.gcf()
 fig.subplots_adjust(wspace=0.02, hspace=0.15)
-fig.set_size_inches(6, 7)
+fig.set_size_inches(9, 4)
 plot_name = 'time-bhmass-1S.png'
 fig.savefig('plots/' + plot_name, bbox_inches='tight')
-plot_name = 'time-bhmass-1S.pdf'
-fig.savefig('plots/' + plot_name, bbox_inches='tight')
-print("created plots/", plot_name)
+plot_name = 'plots/time-bhmass-1S.pdf'
+fig.savefig(plot_name, bbox_inches='tight')
+print("created ", plot_name)
