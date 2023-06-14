c30b288f83982bc534755dc763bf7d3bb4359a4a
Simone G <simonegordon12@icloud.com>
2023-06-14 16:52:00 +0100

    added critical density function

--- python_scripts/plot_ic_attributes.py
+++ python_scripts/plot_ic_attributes.py
@@ -15,6 +15,30 @@ import os
 from matplotlib import rc
 
 
+def critical_density(ds, n_crit=False):
+    # Get the cosmological parameters from the dataset parameters
+    hubble_constant = yt.YTQuantity(ds.parameters.get("CosmologyHubbleConstantNow")*100, "km/s/Mpc")
+    matter_density = yt.YTQuantity(ds.parameters.get("CosmologyOmegaMatterNow"), "")
+    cosmological_constant_density = yt.YTQuantity(ds.parameters.get("CosmologyOmegaLambdaNow"), "")
+
+    # Convert the Hubble constant to CGS units (cm/s/Mpc)
+    hubble_constant_cgs = (hubble_constant).to("cm/s/Mpc")
+
+    # Conver Hubble constant to 1/s units
+    hubble_constant_pers = hubble_constant_cgs / yt.YTQuantity(3.1e19*1e5, "cm/Mpc") 
+
+    # Calculate the critical density
+    critical_density = (3 * hubble_constant_pers**2) / (8 * np.pi * yt.physical_constants.G) * (matter_density + cosmological_constant_density)
+
+    # Convert the critical density to the desired units (1/cm^3)
+    critical_density = critical_density.to("g/cm**3")
+
+    # Convert to hydrogen nuclei density if desired
+    if n_crit:
+        critical_density /= yt.physical_constants.mh # /cm**3
+
+    return critical_density
+
 ################################## Parameters ##################################
 rvir_pc = 2167.60571289 # at z = 26.338 at l3
 M200 = 2.7491550e+05 # at z = 26.338 at l3 (same as Mvir)
@@ -22,7 +46,7 @@ n_subplots = 4
 y = sys.argv[-1] # naming plot
 fontsize = 12 # for projection annotations
 linewidth = 2
-r_lim_kpc = 5 # kpc
+r_lim_kpc = 10 # kpc
 
 root_dir = ["/disk14/sgordon/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/",
             "/disk14/sgordon/cirrus-runs-rsync/seed2-bh-only/270msun/replicating-beckmann-2/"]
@@ -68,26 +92,39 @@ for i, ds in enumerate(DS):
     rp = yt.create_profile(
         sp,
         "radius",
-        [("gas", "mass"), ("gas", "temperature"), ("gas", "dynamical_time"), ("gas", "baryon_overdensity_sg"),
-         ("gas", "blackhole_freefall_timescale"), ("gas", "theta_vel_dynamical_timescale"), ("deposit", "all_mass") ],
-         units={"radius": "pc", ("gas", "mass"): "Msun", ("deposit", "all_mass"): "Msun"},
+        [("gas", "mass"), ("gas", "temperature"),
+        ("deposit", "all_mass"),
+         #("gas", "theta_vel_dynamical_timescale"), 
+         ],
+        units={"radius": "pc", ("gas", "mass"): "Msun", ("deposit", "all_mass"):"Msun"},
         logs={"radius": True, ("gas", "mass"): True, ("gas", "temperature"): True, ("deposit", "all_mass"):True},
-        weight_field=None
+        weight_field=None,
+        accumulation=True # -True to reverse direction
     )
 
     rp2 = yt.create_profile(
         sp,
         "radius",
         [("gas", "temperature"), ("gas", "mass"), ("gas", "dynamical_time"), ("gas", "baryon_overdensity_sg"),
-         ("gas", "number_density"), ("gas", "blackhole_freefall_timescale"), ("gas", "theta_vel_dynamical_timescale")],
-        units={"radius": "pc", ("gas", "mass"): "Msun"},
+         ("gas", "number_density"), ("gas", "blackhole_freefall_timescale"), ("gas", "theta_vel_dynamical_timescale"), ("gas", "density"),
+         ('enzo', 'Dark_Matter_Density')],
+        units={"radius": "pc", ("gas", "mass"): "Msun", ('enzo', 'Dark_Matter_Density'): "g/cm**3"},
         logs={"radius": True, ("gas", "temperature"): True}
     )
 
+    # rp3 = yt.create_profile(
+    #     sp,
+    #     "radius",
+    #     [("all", "particle_mass") ],
+    #      units={"radius": "pc", ("all", "particle_mass"): "Msun"},
+    #     logs={"radius": True},
+    #     weight_field=None
+    # )
+
     # plot data
-    axs[0].loglog(rp.x.value, rp[("gas", "mass")].value.cumsum() + rp[("deposit", "all_mass")].value.cumsum(),
+    axs[0].loglog(rp.x.value, rp[("gas", "mass")].value + rp[("deposit", "all_mass")].value,
                color=c[i], linestyle='solid', label=labels[i] + "_total", alpha=alpha)
-    axs[0].loglog(rp.x.value, rp[("deposit", "all_mass")].value.cumsum(),
+    axs[0].loglog(rp.x.value, rp[("deposit", "all_mass")].value,
                 color=c2[i], linestyle='solid', label= labels[i] + "_DM", alpha=alpha)
     r200 = rp[("deposit", "all_mass")].value.cumsum().max() + rp[("gas", "mass")].value.cumsum().max()
     print("r200 mass: ", r200)
@@ -104,8 +141,10 @@ for i, ds in enumerate(DS):
     # axs[3].loglog(rp2.x[rp2.used], rp2[("gas", "baryon_overdensity_sg")][rp2.used],
     #               color=c[i], linestyle='solid', label=labels[i], alpha=alpha)
 
-    axs[3].loglog(rp2.x[rp2.used], rp2[("gas", "number_density")][rp2.used],
+    axs[3].loglog(rp2.x[rp2.used], rp2[("gas", "density")][rp2.used] + rp2[("enzo", "Dark_Matter_Density")][rp2.used],
                   color=c[i], linestyle='solid', label=labels[i], alpha=alpha)
+    axs[3].loglog(rp2.x[rp2.used], rp2[("enzo", "Dark_Matter_Density")][rp2.used],
+                color=c2[i], linestyle='solid', label=labels[i], alpha=alpha)
 
     # t_dyn is larger when using non-mass-weighted densities
     # axs[2].loglog(rp.x[rp.used], rp[("gas", "dynamical_time")][rp.used].to("yr"),
@@ -142,11 +181,12 @@ axs[0].set_ylabel(r"$\rm M_{encl} \, (M_{\odot})$", fontdict=None)
 axs[0].axhline(y=M200, color='grey', linestyle='dotted', lw=linewidth, alpha=1, label=r"$M_{200/vir}$")
 axs[0].legend(loc="upper left", fontsize=fontsize-4, ncol=1)  # upper/lower
 axs[0].set_title("Gas properties at time of BH formation", fontdict=None)
+axs[3].axhline(y=360*critical_density(ds), color='grey', linestyle='dotted', lw=linewidth, alpha=1, label=r"$n_{200}$")
 
 # save plot as pdf
 fig = plt.gcf()
 fig.subplots_adjust(wspace=0, hspace=0)
 fig.set_size_inches(4.6, 6.2)
-plot_name = 'radial_profile_ics_halo_5kpc.pdf'
+plot_name = 'radial_profile_ics_halo_{}kpc.pdf'.format(r_lim_kpc)
 fig.savefig('plots/' + plot_name, bbox_inches='tight')
 print("created plots/" + str(plot_name))
