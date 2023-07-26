266bcecbc3b46b5a3280d35da9b796b617cb1e28
Simone G <simonegordon12@icloud.com>
2023-07-26 22:45:28 +0100

    refactoring movie script

--- python_scripts/projection_movie_temp.py
+++ python_scripts/projection_movie_temp.py
@@ -1,140 +1,139 @@
-"""
-Create projection images from simulation to be made into a movie. Call like:
-mpirun -np 16 python projection_movie.py
-"""
-
-import sys
 import yt
 import os
+import numpy as np
 yt.enable_parallelism()
+from yt.utilities.math_utils import ortho_find
 from smartstar_find import ss_properties
 from plot_disc_projections import _make_disk_L
-from yt.utilities.math_utils import ortho_find
 from plot_multi_projections import tidy_data_labels
 import matplotlib.pyplot as plt
 from matplotlib import rc
-#from yt.extensions.p2p import add_p2p_fields, add_p2p_particle_filters
 
-
-def apply_annotations_and_save(p, title=None):
-    # set text coords
+def apply_annotations_and_save(p, title=None, dirname=None):
+    # Set text coords
     a = 0.03
     b = 0.95
     b2 = 0.03
     p.set_axes_unit('pc')
 
-    # font size
+    # Font size
     p.set_font({"size": 24})
 
     # BH position cross
     p.annotate_marker(center, coord_system="data", color="white")
 
-    # top left text
+    # Top left text
     p.annotate_text((a, b), r"BH Mass: {:.0f} $\rm M_\odot$".format(ss_mass.d), coord_system="axis",
-                        text_args={"color": "black"}) 
+                    text_args={"color": "white"}) 
     p.annotate_text((a, b-0.05), "BH Age = {:.2f} Myr".format(ss_age[0] / 1e6), coord_system="axis",
-                        text_args={"color": "black"})
+                    text_args={"color": "white"})
     
-    # lower right text
+    # Lower right text
     p.annotate_text((0.82, b2), "z = {:.2f}".format(ds.current_redshift), coord_system="axis",
-                        text_args={"color": "white"})
+                    text_args={"color": "white"})
     p.annotate_text([0.05, 0.05], sim_str, coord_system="axis", text_args={"color": "black"},
                     inset_box_args={"boxstyle": "square,pad=0.3", "facecolor": "white", "linewidth": 3,
-                                    "edgecolor": "white", "alpha": 0.5},
-                    )
+                                    "edgecolor": "white", "alpha": 0.5})
     if title:
         p.annotate_title(str(title))
 
-    dirname = "frames_" + orient_str + field + "_" + str(sim_str) + "_" + str(w_pc) + "pc/"
     p.save(dirname)
 
-    return ("saved to frames to {}".format(dirname))
-            
+def extract_simulation_name(filepath):
+    # Get the last part of the path
+    last_part = os.path.basename(filepath)
 
-def extract_ultimate_directory(filepath):
-    # split the filepath into directory and file components
-    directory, filename = os.path.split(filepath)
+    # Use regular expression to extract the full simulation name
+    match = re.search(r'\b(?:\d+[A-Za-z]+\d+|[A-Za-z]+\d+)\b', last_part)
 
-    # split the directory component into its path elements
-    path_elements = directory.split(os.path.sep)
+    if match:
+        return match.group(0)
 
-    # return the penultimate element, or None if not found
-    return path_elements[-1] if len(path_elements) > 1 else None
+    # If the match is not found, try to extract from the parent directories
+    path_parts = filepath.split(os.path.sep)
+    for i in range(len(path_parts)-1, -1, -1):
+        match = re.search(r'\b(?:\d+[A-Za-z]+\d+|[A-Za-z]+\d+)\b', path_parts[i])
+        if match:
+            return path_parts[i]
 
+    return None
 
-##################################  Parameters ###################################
+def make_projection_map(ds, map_type, field, w_pccm, w_pc, c_min, c_max, dirname):
+    sim_str = tidy_data_labels(extract_simulation_name(ds.fullpath))
+    if map_type == "density":
+        # H nuclei number density
+        p = yt.ProjectionPlot(ds, dir, ("gas", field), width=(w_pc, 'pc'), north_vector=north, center=center, data_source=region,
+                            weight_field=("gas", field))
+        p.set_cmap(field, 'viridis')
+        p.set_zlim(("gas", field), c_min, c_max)
+        apply_annotations_and_save(p, dirname=dirname)
+    
+    elif map_type == "temperature":
+        # Temperature
+        p = yt.ProjectionPlot(ds, dir, ("gas", field), width=(w_pccm, 'pccm'), north_vector=north, center=center, data_source=region,
+                            weight_field=("gas", "density"))
+        p.set_cmap(field, "RED TEMPERATURE")
+        p.set_zlim(("gas", field), 50, 1200)
+        p.set_axes_unit('pc')
+        apply_annotations_and_save(p, dirname=dirname)
 
-# 1) set map variable (density or temperature or metallicity or h2)
-map = "temperature"
+    elif map_type == "metallicity":
+        # Metallicity
+        p = yt.ProjectionPlot(ds, dir, ("enzo", field), width=(w_pccm, 'pccm'), north_vector=north, center=center, data_source=region, 
+                            weight_field=("gas", "density"))
+        p.set_cmap(field, 'kamae')
+        #p.set_zlim(("enzo", field), c_min, c_max)
+        p.set_axes_unit('pc')
+        apply_annotations_and_save(p, dirname=dirname)
 
-# 2) set image orientation (face-on or edge-on) used to produce north vector
-orient = "face-on"
+    elif map_type == "h2":
+        # H2 fraction
+        p = yt.ProjectionPlot(ds, dir, ("gas", field ), width=(w_pccm,'pccm'), north_vector=north, center=center, data_source=region, 
+                            weight_field='cell_mass')
+        p.set_cmap(field, "kelp")
+        p.set_axes_unit('pc')
+        apply_annotations_and_save(p, dirname=dirname)
 
-# 3) set width of box
-w_pccm = 200
-#w_pccm = 30
-w_pc = 8 # convert to pc for label
-#w_pc = 1 # convert to pc for label
-
-# 4) set colorbar limits
-c_min = 3e2
-c_max = 8e9
-
-# 5) data
-#root_dir = "/disk14/sgordon/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/1B.RSb08" 
-root_dir = "/disk14/sgordon/cirrus-runs-rsync/seed1-bh-only/40msun/replicating-beckmann/1S.RSmf8"
-enzo_file = "smartstar-production-runs.enzo"
-sim = os.path.join(root_dir, enzo_file)
-
-#################################################################################
-
-sim_str = tidy_data_labels(extract_ultimate_directory(sim))
-
-if __name__ == "__main__":
+def main(root_dir, map="density", w_pccm=200, w_pc=1.5, c_min=None, c_max=None, orient="face-on"):
+    sim = os.path.join(root_dir, "smartstar-production-runs.enzo")
     es = yt.load_simulation(sim, "Enzo", find_outputs=True)
     es.get_time_series()
 
-    nested_level = es.parameters["CosmologySimulationNumberOfInitialGrids"] - 1
+    nested_level = es.parameters.get("CosmologySimulationNumberOfInitialGrids", 0) - 1
     if nested_level == 0:
         region = None
         center0 = [0.5]*3
         width = None
     else:
-        left_edge = es.parameters["CosmologySimulationGridLeftEdge[%d]" % nested_level]
-        right_edge = es.parameters["CosmologySimulationGridRightEdge[%d]" % nested_level]
+        left_edge = es.parameters.get("CosmologySimulationGridLeftEdge[%d]" % nested_level, np.zeros(3))
+        right_edge = es.parameters.get("CosmologySimulationGridRightEdge[%d]" % nested_level, np.zeros(3))
         my_width = (right_edge - left_edge).max()
         center0 = 0.5 * (left_edge + right_edge)
 
-    # find north vector a (near-end) ds
-    ds_final = yt.load(os.path.join(root_dir, "DD0136/DD0136"))
+    # Find north vector at (near-end) ds
+    ds_final = yt.load(os.path.join(root_dir, "DD0143/DD0143"))
     ss_pos, ss_mass, ss_age = ss_properties(ds_final)
     center = ss_pos
-    r = 2000*yt.units.pc
+    r = 2000 * yt.units.pc
     sp = ds_final.sphere(center, 2 * r)
 
-    # make disk data container and define angular momentum vector L
+    # Make disk data container and define angular momentum vector L
     disc_r_pc = 2.1
     disc_h_pc = 2.1
     disk, L = _make_disk_L(ds_final, ss_pos, disc_r_pc, disc_h_pc)
     
-    # Gives a 3d vector and it will return 3 orthogonal vectors, the first one being the original vector
-    # and the 2nd and 3rd being two vectors in the plane orthogonal to the first and orthogonal to each other.
-    # It's very useful for giving you a vector edge-on to the disk.
+    # Get orthogonal vectors for face-on or edge-on projection
     vecs = ortho_find(L)
     v = 0
 
     if orient == "face-on":
-        orient_str = "face_on_"
         dir = vecs[0]
         north = vecs[1]
     else:
-        orient_str = "edge_on_"
         dir = vecs[1]
         north = vecs[0]
     
-
-    for ds in es.piter():                                                                                                                                                                               
-
+    for ds in es.piter():
         ss_pos, ss_mass, ss_age = ss_properties(ds)
         center = ss_pos
 
@@ -143,47 +142,13 @@ if __name__ == "__main__":
                             center0 + 0.5 * my_width)
             width = (my_width, "unitary")
 
-        if map == "density":
-            # H nuclei number density
-            field = "number_density"
-            p1 = yt.ProjectionPlot(ds, dir, ("gas", field), width=(w_pccm, 'pccm'), north_vector=north, center=center, data_source=region,
-                                   weight_field=("gas", field))
-            p1.set_cmap(field, 'viridis')
-            p1.set_zlim(("gas", field), c_min, c_max)
+        dirname = "frames_" + orient + field + "_" + str(sim_str) + "_" + str(w_pc) + "pc/"
+        os.makedirs(dirname, exist_ok=True)
 
-            apply_annotations_and_save(p1)
-        
-        elif map == "temperature":
-            # Temperature
-            field = "temperature"
-            p2 = yt.ProjectionPlot(ds, dir, ("gas", field), width=(w_pccm, 'pccm'), north_vector=north, center=center, data_source=region,
-                                   weight_field=("gas", "density"))
-            p2.set_cmap(field, "RED TEMPERATURE")
-            p2.set_zlim(("gas", field), 50, 1200)
-            p2.set_axes_unit('pc')
+        make_projection_map(ds, map, field, w_pccm, w_pc, c_min, c_max, dirname)
 
-            apply_annotations_and_save(p2)
+    print("Saved data to", dirname)
 
-        elif map == "metallicity":
-
-            # add pop3 metallicity fields
-            # add_p2p_fields(ds) 
-
-            # Metallicity
-            field = "SN_Colour"
-            p3 = yt.ProjectionPlot(ds, "x", ("enzo", field), width=(w_pccm, 'pccm'), north_vector=north, center=center, data_source=region,
-                                   weight_field=("gas", "density"))
-            p3.set_cmap(field, 'kamae')
-            p3.set_axes_unit('pc')
-
-            apply_annotations_and_save(p3)
-
-        elif map == "h2":
-            # H2 fraction
-            field = "H2_p0_fraction"
-            p4 = yt.ProjectionPlot(ds, "x", ("gas", field ), north_vector=north, width=(w_pccm,'pccm'), center=center, data_source=region, 
-                                   weight_field='cell_mass')
-            p4.set_cmap(field, "kelp")
-            p4.set_axes_unit('pc')
-
-            apply_annotations_and_save(p4)
+if __name__ == "__main__":
+    root_dir = "/disk14/sgordon/cirrus-runs-rsync/seed1-bh-only/40msun/replicating-beckmann/1S.RSm04" 
+    main(root_dir, map="density", w_pccm=200, w_pc=1.5, c_min=None, c_max=None, orient="face-on")
