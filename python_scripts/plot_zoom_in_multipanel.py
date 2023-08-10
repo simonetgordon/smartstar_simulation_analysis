114cc8da142c5110606d44f9324d0e6b55eee5da
Simone G <simonegordon12@icloud.com>
2023-08-10 15:48:18 +0100

    zoom multipanel final touches. Produces temp plot well, but min_n for colorbar may need adjustment for density projections

--- python_scripts/plot_zoom_in_multipanel.py
+++ python_scripts/plot_zoom_in_multipanel.py
@@ -9,7 +9,7 @@ import matplotlib.ticker as ticker
 from yt.utilities.math_utils import ortho_find
 import re
 from smartstar_find import ss_properties
-from plot_multipanel_time_2 import create_axes_grid, get_min_max_values, configure_projection_plot, configure_font, make_projection_plot, set_ticks_and_labels
+from plot_multipanel_time_2 import create_axes_grid, get_min_max_values, configure_font, make_projection_plot, set_ticks_and_labels
 from plot_disc_projections import _make_disk_L
 from contextlib import redirect_stdout
 
@@ -33,6 +33,7 @@ def format_sci_notation(x):
     a, b = '{:.2e}'.format(x).split('e')
     return r'$\rm {} \times 10^{{{}}}$'.format(a, int(b))
 
+
 def find_smallest_cell_width(ds):
     # find smallest cell width
     with open('ds_stats.txt', 'w+') as f:
@@ -47,14 +48,13 @@ def find_smallest_cell_width(ds):
     return dx
 
 
-def main(root_dir, sim, dds, field, k, widths_pccm, fontsize, orient="face-on"):
+def main(root_dir, sim, dds, field, k, widths_pccm, fontsize, min_n_factor, max_n_factor, orient="face-on", cmap="viridis"):
     configure_font()
     fig = plt.figure()
     grid = create_axes_grid(fig, nrows=3, ncols=1, dim=(0, 0, 0.3, 0.85), 
                             axes_pad=0.3, share_all=False, cbar_size="3%", cbar_pad="0%")
 
-    min_n, max_n, min_n_index, max_n_index = get_min_max_values(root_dir, sim, dds, field, 
-                                                                min_n_factor=1e3, max_n_factor=0.1)
+    min_n, max_n, min_n_index, max_n_index = get_min_max_values(root_dir, sim, dds, field, min_n_factor=min_n_factor, max_n_factor=max_n_factor)
 
     for i, dd in enumerate(dds):
 
@@ -77,26 +77,27 @@ def main(root_dir, sim, dds, field, k, widths_pccm, fontsize, orient="face-on"):
 
         # set north vector and v
         vecs = ortho_find(L)
-        north = vecs[1] if orient=="face-on" else vecs[0]
-        v = 0 if orient=="face-on" else 1
-        if north[-1] < 0:
-            north *= [1,1,-1]
+        north = vecs[0] if orient=="face-on" else vecs[0]
+        v = 1 if orient=="face-on" else 0
+        # if north[-1] < 0:
+        #     north *= [1,1,-1]
 
+        print("min_n: {}, max_n: {}".format(min_n, max_n))
         # make projection plot
-        p = make_projection_plot(ds, width, disk, L, field, vecs, v, north, min_n, max_n, fontsize)
+        p = make_projection_plot(ds, width, disk, L, field, vecs, v, north, min_n, max_n, fontsize, cmap)
 
         # find smallest cell width
         dx = find_smallest_cell_width(ds)
 
-        # This section takes care of annotating the projections based on its position
+        # This section takes care of annotating the projections based on its column
         if k == 0:
             p.annotate_title("dx = {} pc".format(format_sci_notation(float(dx))))
             p.annotate_text([0.07, 0.08], str(label), coord_system="axis", text_args={"color": "black"},
                             inset_box_args={"boxstyle": "square,pad=0.3", "facecolor": "white", "linewidth": 3,
                                             "edgecolor": "white", "alpha": 0.5})
-        if k == 1:
+        elif k == 1:
             p.annotate_title("BH Age = {:.2f} Myr".format(ss_age[0] / 1e6))
-        if k == 2:
+        elif k == 2:
             p.annotate_marker(center, coord_system="data", color="black")
             p.annotate_title(r"BH Mass: {} $\rm M_\odot$".format(int(ss_mass.d)))
 
@@ -130,34 +131,48 @@ def main(root_dir, sim, dds, field, k, widths_pccm, fontsize, orient="face-on"):
         if (i == 2) and (k != 0): # i=1 -> i=2 temp
             plot.axes.get_yaxis().set_units('pc')
 
-        if k == 2: #temp 2 -> 0
-            grid.cbar_axes[i].set_ylabel(r'Number Density \big($\rm \frac{1}{cm^{3}}$\big)', fontsize=14)
-            grid.cbar_axes[i].minorticks_on()
+        if k == 2: 
+            if field == "temperature":
+                grid.cbar_axes[i].set_ylabel(r'Temperature \big($\rm K$\big)', fontsize=14)
+                grid.cbar_axes[i].minorticks_on()
+            else:
+                grid.cbar_axes[i].set_ylabel(r'Number Density \big($\rm \frac{1}{cm^{3}}$\big)', fontsize=14)
+                grid.cbar_axes[i].minorticks_on()
 
-            # make minorticks
-            a1 = np.logspace(min_n_index, max_n_index, np.abs(min_n_index - max_n_index)+1)
-            a2 = np.arange(1, 10, 1)
-            minorticks = np.outer(a1, a2).flatten()
-            atol = 10**(max_n_index-1)
-            end_i = first_index(minorticks, float(max_n.d), rtol=0.1, atol=atol)
-            minorticks = minorticks[:end_i]
-            grid.cbar_axes[i].yaxis.set_minor_locator(ticker.AutoMinorLocator())
-            grid.cbar_axes[i].set_yticks(minorticks, minor=True)
-            grid.cbar_axes[i].tick_params(labelsize=14)
+                # make minorticks
+                start_value = np.log10(min_n)
+                a1 = np.logspace(min_n_index, max_n_index, np.abs(min_n_index - max_n_index)+1)
+                a2 = np.arange(1, 10, 1)
+                minorticks = np.outer(a1, a2).flatten()
+                atol = 10**(max_n_index-1)
+                end_i = first_index(minorticks, float(max_n.d), rtol=0.1, atol=atol)
+                minorticks = minorticks[:end_i]
+                grid.cbar_axes[i].yaxis.set_minor_locator(ticker.AutoMinorLocator())
+                grid.cbar_axes[i].set_yticks(minorticks, minor=True)
+                grid.cbar_axes[i].tick_params(labelsize=14)
 
-    plot_name_prefix = f"multiplot_axesgrid_{widths.d[k]}pccm"
+    plot_name_prefix = f"multiplot_axesgrid_{widths.d[k]}pccm_{field}"
     plt.savefig('plots/' + plot_name_prefix + '.pdf', bbox_inches='tight')
     print("created plots/{}.pdf".format(plot_name_prefix))
 
 if __name__ == "__main__":
 
-    # Initialization
+    # Initialisation
     root_dir = ["/cephfs/sgordon/disk14/cirrus-runs-rsync/seed1-bh-only/270msun/replicating-beckmann/"] * 3
     sim = ["1B.RSb01-2", "1B.RSb04", "1B.RSb16"]
     dds = ["DD0138/DD0138", "DD0138/DD0138", "DD0167/DD0167"]
-    field = "number_density"
+
+    # column width in pc
     k = int(sys.argv[-1])
-    widths_pccm = [3500, 80, 10]  # Example widths
+    widths_pccm = [3500, 80, 10]
     fontsize = 12
 
-    main(root_dir, sim, dds, field, k, widths_pccm, fontsize, orient="face-on")
+    # set field, cmap
+    field = "temperature" # "temperature" or "number_density
+    cmap = "magma" # "RED TEMPERATURE" or "hot" or "inferno" or "magma" (temp) or "viridis" (density)
+
+    # set to reduce colorbar limits
+    min_n_factor = 50 # 50 for temp, 1e3 for density
+    max_n_factor = 0.2 # 0.1 for temp+density
+
+    main(root_dir, sim, dds, field, k, widths_pccm, fontsize, min_n_factor=min_n_factor, max_n_factor=max_n_factor, orient="face-on", cmap=cmap)
