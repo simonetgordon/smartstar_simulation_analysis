b5f85e63c6289c09d7d0d6a570d590cce9763fca
Simone G <simonegordon12@icloud.com>
2024-04-03 13:45:15 +0100

    delete master clump identification routine

--- python_scripts/make_clump_ds.py
+++ python_scripts/make_clump_ds.py
@@ -1,11 +1,32 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import rc
 import numpy as np
 import seaborn as sns
 import yt
-from yt.data_objects.level_sets.api import * # for clump finding
+from yt.data_objects.level_sets.api import *
+import yt.units # for clump finding
 from helper_functions import *
 from helper_functions import _h2_fraction
 
+
+def extract_specific_part(filepath):
+    # Define a regex pattern to capture the segment before the last two 'DD0---' parts
+    # This pattern assumes the 'DD0---/DD0---' part is consistent at the end
+    pattern = r'/(.*?)/DD0.+/DD0.+$'
+    
+    # Search for the pattern in the filepath
+    match = re.search(pattern, filepath)
+    
+    # If a match is found, return the captured group; otherwise, return None
+    if match:
+        # The captured group is everything before the specified pattern, potentially including other path segments
+        # So, we split it by '/' and take the last part
+        specific_part = match.group(1).split('/')[-1]
+        return specific_part
+    else:
+        return None
+    
 # Add information to the master clump
 def _mass_weighted_jeans_mass(clump, units="Msun"):
     # Assuming this calculates a scalar value correctly
@@ -43,29 +64,40 @@ def process_leaves(master_clump):
     print("Least massive leaf clump mass = {:.5f} (clump id {})".format(min(leaf_masses), leaf_clumps[np.argmin(leaf_masses)].clump_id))
     return leaf_masses
 
-def plot_histogram(leaf_masses, sim, dd, nbins=50, xlabel="Mass ($M_\odot$)", ylabel="Number of Clumps", logscale=False):
+def plot_histogram(leaf_masses, sim, dd, ss_age, c_min, nbins=50, xlabel="Mass ($M_\\odot$)", ylabel="Number of Clumps", logscale=False):
     # Create the histogram using Seaborn
     plt.figure(figsize=(8, 6))
     leaf_masses_values = np.array([mass.value for mass in leaf_masses])
     ax = sns.histplot(leaf_masses_values, bins=nbins, color='pink', kde=False)
-    # # Get the counts and bin edges
+
+    # Calculate min, max, and number of leaves
+    min_mass = np.min(leaf_masses_values)
+    max_mass = np.max(leaf_masses_values)
+    num_leaves = len(leaf_masses_values)
+
+    # Get the counts and bin edges
     counts, bins = np.histogram(leaf_masses, bins=20)
     # Annotate with the counts in each bin
     for count, bin in zip(counts, bins[:-1]):  # Iterate over counts and left edges of bins
         bin_center = bin + (bins[1] - bins[0]) / 2  # Calculate center of bin
         ax.text(bin_center, count, str(int(count)), ha='center', va='bottom')
 
-    plt.title('Distribution of Leaf Clump Masses in Simulation {} at {}'.format(sim, dd))
+    # Add text for min, max, and number of leaves to the top right
+    text_str = f"Min Mass: {min_mass:.2e} $M_\\odot$\nMax Mass: {max_mass:.2e} $M_\\odot$\nNumber of Leaves: {num_leaves}\n Min Density: {c_min/mh:.2e} cm$^{-3}$"
+    plt.text(0.95, 0.95, text_str, transform=ax.transAxes, horizontalalignment='right', verticalalignment='top', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+
+    plt.title('Distribution of Leaf Clump Masses in Simulation {} at {:.2f} Myr, grav bound = {}'.format(sim, ss_age[0]/1e6, GRAVITATIONALLY_BOUND))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     #plt.yscale('log') if logscale else None  # Optional: Use log scale for y-axis
     plt.xscale('log') if logscale else None 
     plt.grid(True)  # Optional: Adds a grid for better readability
-    plt.savefig(f'plots/leaf_clump_masses_hist_{sim}_{dd}.png') 
-    print(f"Saved plots/leaf_clump_masses_hist_{sim}_{dd}.png")
+    plot_name = f"plots/clump_masses_hist_{sim}_{dd}_{c_min.d/mh.d:.2e}_grav_bound.png" if GRAVITATIONALLY_BOUND else f"plots/clump_masses_hist_{sim}_{dd}_{c_min.d/mh.d:.2e}.png"
+    plt.savefig(plot_name) 
+    print(plot_name)
     return 0
 
-def process_master_clump(master_clump, ds, ss_pos, ss_age, sim, dd):
+def process_master_clump(master_clump, ds, ss_pos, ss_age, ss_mass, sim, dd, c_min):
     # Get a list of just the leaf nodes.
     leaf_clumps = master_clump.leaves
 
@@ -79,7 +111,7 @@ def process_master_clump(master_clump, ds, ss_pos, ss_age, sim, dd):
         p.set_zlim('number_density', 4e21, 8e25)
         p.annotate_timestamp(corner='upper_right', redshift=True, draw_inset_box=True)
         p.annotate_clumps(leaf_clumps, cmap="Pastel1")
-        p.annotate_clumps([most_massive_clump], text_args={"color": "red"})
+        p.annotate_clumps([most_massive_clump], text_args={"color": "black"})
         most_massive_clump_mass = most_massive_clump.info["cell_mass"][1]
         most_massive_clump_pos = most_massive_clump.info["position"][1].to('pc')
         text_string = f"Most Massive Clump: {most_massive_clump_mass:.2f}\n Total Clump Mass: {sum(leaf_masses):.0f}\n Number of Clumps: {len(leaf_clumps)}\n"
@@ -88,75 +120,76 @@ def process_master_clump(master_clump, ds, ss_pos, ss_age, sim, dd):
                         inset_box_args={"boxstyle": "square,pad=0.3", "facecolor": "white", "linewidth": 3, 
                                         "edgecolor": "white", "alpha": 0.5}
                         )
-        p.annotate_text((0.82, 0.05), "BH Age = {:.2f} Myr".format(ss_age[0]/1e6), coord_system="axis", text_args={"color": "white"})
-        p.annotate_marker(master_clump.info["position"][1], coord_system='data', color='white', s=100)
-        p.save(f"plots/clump_projection_{sim}_{dd}_{i}.png")
-        print(f"Saved plots/clump_projection_{sim}_{dd}_{i}.png")
+        p.annotate_text((0.68, 0.02), "BH Age = {:.2f} Myr".format(ss_age[0]/1e6), coord_system="axis", text_args={"color": "white"})
+        p.annotate_text((0.68, 0.06), r"BH Mass: {:.2f} $\rm M_\odot$".format(ss_mass.d), coord_system="axis", text_args={"color": "white"}) 
+        #p.annotate_marker(ss_pos, coord_system='data', color='white', s=100)
+        plot_name = f"plots/clump_projection_{sim}_{dd}_{i}_{c_min.d/mh.d:.2e}_grav_bound.png" if GRAVITATIONALLY_BOUND \
+            else f"plots/clump_projection_{sim}_{dd}_{i}_{c_min.d/mh.d:.2e}.png"
+        p.save(plot_name)
+        print(plot_name)
 
     # plot histogram
-    plot_histogram(leaf_masses, sim, dd, logscale=False)
+    plot_histogram(leaf_masses, sim, dd, ss_age, c_min, logscale=False)
 
     return 0
     
 
 if __name__ == "__main__":
 
-    global LOCATE_MASTER_CLUMP
-    LOCATE_MASTER_CLUMP = False
+    # Set up the plotting environment
+    rc('font', **{'family': 'serif', 'serif': ['Times'], 'weight': 'light'})
+    plt.rcParams["mathtext.default"] = "regular"
+    mpl.rcParams['text.usetex'] = True
+
+    global GRAVITATIONALLY_BOUND
+    global mh 
+    GRAVITATIONALLY_BOUND = True
+    mh = 1.6735e-24*yt.units.g # hydrogen mass in grams
 
     # load the dataset
-    fp = "/disk14/sgordon/pleiades-11-12-23/seed1-bh-only/270msun/replicating-beckmann/1B.RSb01-2/DD0446/DD0446"
+    #fp = "/disk14/sgordon/pleiades-11-12-23/seed1-bh-only/270msun/replicating-beckmann/1B.RSb01-2/DD0449/DD0449"
     #fp = "/disk14/sgordon/pleiades-11-12-23/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eps-0.0001/DD0522/DD0522"
-    #fp = "/disk01/sgordon/pleiades-18-03-24/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eta-0.01/DD0517/DD0517"
+    #fp = "/disk01/sgordon/pleiades-18-03-24/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eps-0.001/DD0514/DD0514"
+    #fp = "/disk01/sgordon/pleiades-18-03-24/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eta-0.01/DD0519/DD0519"
+    #fp = "/disk01/sgordon/pleiades-18-03-24/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eta-0.1/DD0596/DD0596"
+    #fp = "/disk01/sgordon/pleiades-18-03-24/seed1-bh-only/270msun/thermal-fb/1B.resim.th.b01-3-eta-0.1/DD0612/DD0612"
+    fp = "/nobackup/stgordon/seed1-bh-only/270msun/thermal-fb/1B.th.bf128"
+    fp = "/disk14/sgordon/pleiades-11-12-23/seed1-bh-only/270msun/thermal-fb/1B.th.bf128-eps-0.0001/DD0141/DD0141"
     ds = yt.load(fp)
-    sim = "1B.b01"
+    #sim ="1B.b01" # may need to adjust this
+    sim = extract_specific_part(fp)
     dd = extract_dd_segment(fp)
     print("Simulation: {} and DD: {} at time: {}".format(sim, dd, ds.current_time.to('Myr')))
     ds.add_field(("gas", "h2_fraction"), function=_h2_fraction, units="dimensionless", display_name="H2 Fraction", sampling_type="cell")
 
     # find sink particle attribute
     ss_pos, ss_mass, ss_age = ss_properties(ds, velocity=False)
-    print("Sink particle properties:")
+    print("BH properties:")
     print("position = {}, mass = {:.2f}, age = {:.2f} Myr".format(ss_pos, ss_mass, ss_age[0]/1e6))
 
-    # plot the density projection in each direction to locate the master clump by eye
-    if LOCATE_MASTER_CLUMP:
-        for i in range(3):
-            p = yt.ProjectionPlot(ds, i, ('gas','number_density'), center=ss_pos, width=(5, 'pc'))
-            p.set_zlim("number_density", 9e-3, 6e2)
-            p.set_cmap('number_density', 'cividis')
-            p.annotate_marker(ss_pos, coord_system='data', color='red', s=100)
-            p.annotate_timestamp(corner='upper_left', redshift=True, draw_inset_box=True)
-            p.save("plots/density_projection_{}.png".format(i))
-            print("Saved plots/density_projection_{}.png".format(i))
-
     # Make initial master clump (a disk containing the clump) - takes 30 seconds
-    clump_pos = ss_pos.to('pc') + [-1.5, -0.5, -1.5]*yt.units.pc
-    data_source = ds.sphere(clump_pos, (2, "pc"))
+    clump_pos = ss_pos.to('pc')
+    data_source = ds.sphere(clump_pos, (3.5, "pc"))
     master_clump = Clump(data_source, ("gas", "density"))
 
     # Add clump info items
     add_clump_info("mass_weighted_jeans_mass", _mass_weighted_jeans_mass)
     add_clump_info("position",  _center_of_mass)
+    #add_clump_info("density",  _density)
     master_clump.add_info_item("position")
     master_clump.add_info_item("mass_weighted_jeans_mass")
 
-    print("------------------------------------")
-    print("Master clump created")
-    print("Master clump position = {}".format(master_clump.info['position']))
-    print("Master clump mass = {}".format(master_clump.info['cell_mass']))
-    print("Master clump mass weighted Jeans mass = {}".format(master_clump.info['mass_weighted_jeans_mass']))
-    print("------------------------------------")
-
     # Add clump validators (refine clump properties)
     #master_clump.add_validator("min_cells", 20)
-    master_clump.add_validator("gravitationally_bound", use_particles=False)
-    master_clump.add_validator("minimum_gas_mass", ds.quan(0.5, "Msun"))
+    master_clump.add_validator("gravitationally_bound", use_particles=False) if GRAVITATIONALLY_BOUND else None # PE > KE
+    master_clump.add_validator("minimum_gas_mass", ds.quan(1, "Msun"))
 
     # Run clump finding
-    c_min = data_source["gas", "density"].min()*3
-    c_max = data_source["gas", "density"].max()
-    step = 2
+    #c_min = 1e2*mh/yt.units.cm**3 # 1e6 for 31Myr resim, 1e4 for 1B.th.bf128
+    #c_max = data_source["gas", "density"].max()
+    c_min = 10 ** np.floor(np.log10(data_source["gas", "density"]).min())*yt.units.g/yt.units.cm**3
+    c_max = 10 ** np.floor(np.log10(data_source["gas", "density"]).max() + 1)*yt.units.g/yt.units.cm**3
+    step = 1.5 # This is the multiplicative interval between contours.
     find_clumps(master_clump, c_min, c_max, step)
 
     # Save the clump tree as a reloadable dataset
@@ -165,4 +198,14 @@ if __name__ == "__main__":
 
     # Process the master clump
     max_leaf_mass = -1
-    process_master_clump(master_clump, ds, ss_pos, ss_age, sim, dd)
+    process_master_clump(master_clump, ds, ss_pos, ss_age, ss_mass, sim, dd, c_min)
+    print("Master clump processed")
+    print("------------------------------------")
+    print("Clump finding parameters:")
+    print("Density range: {:.2e} to {:.2e}".format(c_min, c_max))
+    print("Gravitationally Bound Clumps? {}".format(GRAVITATIONALLY_BOUND))
+    print("Step: {}".format(step))
+    print("Master clump position = {}".format(master_clump.info['position']))
+    print("Master clump mass = {}".format(master_clump.info['cell_mass']))
+    print("Master clump mass weighted Jeans mass = {}".format(master_clump.info['mass_weighted_jeans_mass']))
+    print("------------------------------------")
